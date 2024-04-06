888da54519ef60e7a8f13c36b23195b0a546cd0d
NoshKosh <Jkoshy05@gmail.com>
2024-04-06 13:58:23 -0500

    Co-authored-by: Ben Gordon <ben.gordon@utexas.edu>

--- PoliceApp/src/components/Home.js
+++ PoliceApp/src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import line from '../assets/line.svg';
 import map from '../assets/map.svg';
+import { db } from '../firebase';
+import { onValue, ref } from "firebase/database";
 
 const DateTimeDisplay = ({ currentTime }) => {
   const formatTime = (time) => {
@@ -41,6 +43,20 @@ const DateTimeDisplay = ({ currentTime }) => {
 
 export const Home = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [vehicleStats, setVehicleStats] = useState({available: 0, occupied: 0, away: 0})
+
+  useEffect(() => {
+    const query = ref(db, `/vehicleStats`);
+    return onValue(query, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data)
+      if (snapshot.exists()) {
+        setVehicleStats(data)
+      }
+    });
+  }, []);
+
+
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -60,28 +76,28 @@ export const Home = () => {
           <div className="vehicleHeader">
             <p className="vehicleName">Vehicle</p>
             <div className="blueNotification">
-              <p className="notificationText">241</p>
+              <p className="notificationText">{vehicleStats.available + vehicleStats.occupied + vehicleStats.away}</p>
             </div>
           </div>
           <div className="vehicleStatus">
             <div className="statusContainer">
               <p className="statusType">Available</p>
-              <div className="greenNotification">
-                <p className="notificationText">59</p>
+              <div className="greenNotification vsNotification">
+                <p className="notificationText">{vehicleStats.available}</p>
               </div>
             </div>
             <img src={line} className="vehicleLine" alt="Line" />
             <div className="statusContainer">
               <p className="statusType">Occupied</p>
-              <div className="yellowNotification">
-                <p className="notificationText">162</p>
+              <div className="yellowNotification vsNotification">
+                <p className="notificationText">{vehicleStats.occupied}</p>
               </div>
             </div>
             <img src={line} className="vehicleLine" alt="Line" />
             <div className="statusContainer">
               <p className="statusType">Away</p>
-              <div className="redNotification">
-                <p className="notificationText">20</p>
+              <div className="redNotification vsNotification">
+                <p className="notificationText">{vehicleStats.away}</p>
               </div>
             </div>
           </div>
