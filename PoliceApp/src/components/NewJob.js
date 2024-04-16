4fda2e875ada00a1d4b591d3920ad163ef3c25e8
taylormh432 <taylor.hickman432@gmail.com>
2024-04-15 19:14:35 -0500

    mobile app

--- PoliceApp/src/components/NewJob.js
+++ PoliceApp/src/components/NewJob.js
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
+import { db } from '../firebase';
+import { onValue, ref } from "firebase/database";
 import {
   APIProvider,
   Map,
@@ -13,14 +15,36 @@ import {
 export default function Intro() {
   const position = { lat: 30.2672, lng: -97.74711985445532 };
   const [open, setOpen] = useState(false);
+  const [vehicles, setVehicles] = useState([])
+
+  useEffect(() => {
+    const query = ref(db, `/vehicles`);
+    return onValue(query, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data)
+      if (snapshot.exists()) {
+        setVehicles(Object.values(data))
+      }
+    });
+  }, []);
 
   return (
     <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
       <div style={{ height: "100vh", width: "100%" }}>
         <Map defaultZoom={13} defaultCenter={position} mapId={process.env.NEXT_PUBLIC_MAP_ID}>
+          {
+            vehicles.map((vehicle, index) =>
+              <Marker position={vehicle.location} key={index} onClick={() => setOpen(true)}>
+                <Pin
+                  color={"blue"}
+                  borderColor={"green"}
+                  glyphColor={"purple"}
+                />
+              </Marker>)
+          }
           <Marker position={position} onClick={() => setOpen(true)}>
             <Pin
-              background={"grey"}
+              background={"red"}
               borderColor={"green"}
               glyphColor={"purple"}
             />
@@ -36,9 +60,3 @@ export default function Intro() {
     </APIProvider>
   );
 }
-
-
-
-
-
-
