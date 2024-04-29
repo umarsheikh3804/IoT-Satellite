3ad719450d66c076d441f01a2d87190ebe96f38f
taylormh432 <taylor.hickman432@gmail.com>
2024-04-29 10:43:37 -0500

    mobile dashboard

--- mobile-app/src/App.js
+++ mobile-app/src/App.js
@@ -6,31 +6,24 @@ import Navbar from './components/Navbar';
 import {useState, useEffect} from "react";
 
 function App() {
-  const [page, setPage] = useState("dashboard")
-
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => console.log(pos.coords));
-    } else { 
-      console.log("Geolocation is not supported by this browser.")
-    }
-  }
+  const [page, setPage] = useState("dashboard");
 
   useEffect(() => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }, [])
-
+  }, []);
 
   const router = {
     "dashboard": <Dashboard setPage={setPage}/>,
     "settings": <Settings setPage={setPage}/>,
-  }
+  };
   
-  return <div className='App flexCol'>
-    {router[page]}
-    {(page === "dashboard" || page === "settings") && <Navbar/>}
-  </div>
+  return (
+    <div className='App flexCol'>
+      {router[page]}
+      {(page === "dashboard" || page === "settings") && <Navbar setPage={setPage}/>}
+    </div>
+  );
 }
 
 export default App;
--- mobile-app/src/components/Navbar.js
+++ mobile-app/src/components/Navbar.js
@@ -1,7 +1,17 @@
-function Navbar() {
+import React from 'react';
+import { FaHome, FaCog } from 'react-icons/fa'; // Importing icons from react-icons library
+
+function Navbar({ setPage }) {
   return (
     <div className="navbar">
-      <p>Navbar</p>
+      {/* Home Icon */}
+      <div className="navIcon" onClick={() => setPage("dashboard")}>
+        <FaHome />
+      </div>
+      {/* Settings Icon */}
+      <div className="navIcon" onClick={() => setPage("settings")}>
+        <FaCog />
+      </div>
     </div>
   );
 }
